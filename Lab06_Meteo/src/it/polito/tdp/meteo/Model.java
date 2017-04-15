3e392833ba25c04151fd7695a1b0e599189ab42f
JIMMY <andrea.marcelli@polito.it>
2017-04-15 11:23:12 +0200

    Bug fix and added library

--- Lab06_Meteo/src/it/polito/tdp/meteo/Model.java
+++ Lab06_Meteo/src/it/polito/tdp/meteo/Model.java
@@ -6,7 +6,7 @@ import it.polito.tdp.meteo.bean.SimpleCity;
 
 public class Model {
 
-	private final static int COST = 50;
+	private final static int COST = 100;
 	private final static int NUMERO_GIORNI_CITTA_CONSECUTIVI_MIN = 3;
 	private final static int NUMERO_GIORNI_CITTA_MAX = 6;
 	private final static int NUMERO_GIORNI_TOTALI = 15;
